68b23c6d730981983f33eec75c32b8c95eed8cb6
the-sann <kimsannmao025@gmail.com>
2026-04-25 19:00:07 +0700

    add-new-project

--- src/scenes/project/Project.jsx
+++ src/scenes/project/Project.jsx
@@ -1,6 +1,7 @@
 import Project1 from "./Project1";
 import Project2 from "./Project2";
 import Project3 from "./Project3";
+import Project_Express from "./Project_Express";
 
 const Project = () => {
   return (
@@ -13,6 +14,7 @@ const Project = () => {
         <Project1 />
         <Project2 />
         <Project3 />
+        <Project_Express />
       </div>
     </div>
   );
